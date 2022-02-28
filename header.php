1a020893ac638fcfdf0a64d628ee87ad5a3f7e0c
Agu Christopher Chijikem <83035789+dev-kay1@users.noreply.github.com>
2022-02-28 11:25:36 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!--load all Font Awesome styles -->
+ <script src="https://kit.fontawesome.com/2faa7be403.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="css/index.css">
+  <script src="jquery.js"></script>
+  <script src="js/index.js"></script>
+  <title>Agu C Christopher</title>
+</head>
+<body>
+  <div id="navbar"> 
+  <ul>
+    <li>
+<a href="#" onclick="a()" title="Home" id="a"><i class="fa fa-lg fa-home"></i></a> </li>
+<li>
+<a href="projects.html"  class="rnd"> <i class="fa fa-lg fa-hammer"></i></a></li>
+<li>
+<a title="Profile" href="profile.html"><i class="fas fa-lg fa-circle-user"></i></a></li>
+<li>
+  <a href="#"> <i class="fa fa-lg fa-phone"></i> </a>
+</li>
+	</ul>
+	</div>
+	<br>
+		<br />
+   <h1 id="txt1"> Agu C. Christopher </h1>
