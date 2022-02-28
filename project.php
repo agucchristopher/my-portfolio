d3b1f63de1f6d7f5778915cc02e315318878e078
Agu Christopher Chijikem <83035789+dev-kay1@users.noreply.github.com>
2022-02-28 11:33:17 +0100

    Create project.php

---
+++ project.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!--load all Font Awesome styles -->
+ <script src="https://kit.fontawesome.com/2faa7be403.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="css/index.css">
+  <script src="jquery.js"></script>
+  <script src="js/index.js"></script>
+  <title>Agu C Christopher Projects</title>
+</head>
+<body>
+  <?php include("header.php") ?>
